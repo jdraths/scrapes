274d1ec89920c2ffa11be8218a0c05848f187c43
James Roane Draths <jdraths@alumni.nd.edu>
2020-02-24 06:08:04 -0600

    updated scrapes -> ivolve

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Scrapes`,
+    title: `iVolve`,
     description: `A collection of ideas, lessons-learned, and best practices scraped from everywhere possible`,
     author: `jdraths`,
   },
@@ -43,8 +43,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#001440`,
+        theme_color: `#001440`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
