f36f174c927ac567da044b2d847954d636248dc3
Roane Draths <jdraths@alumni.nd.edu>
2020-04-29 12:50:37 -0500

    deploy

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,9 +1,11 @@
 // from gatsby tutorials
 import Typography from "typography"
-// import kirkhamTheme from "typography-theme-kirkham"
-import oceanBeach from "typography-theme-ocean-beach"
+import kirkhamTheme from "typography-theme-kirkham"
+// import stowLake from "typography-theme-stow-lake"
+// didn't like:
+// import oceanBeach from "typography-theme-ocean-beach"
 
-const typography = new Typography(oceanBeach)
+const typography = new Typography(kirkhamTheme)
 
 export default typography
 export const rhythm = typography.rhythm
